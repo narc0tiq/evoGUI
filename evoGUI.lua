8d56d4aaad6fa40bba432bf21111270f677a9701
Octav Sandulescu <github@narc.ro>
2015-07-31 19:33:31 +0000

    Try another look for the GUI

--- evoGUI.lua
+++ evoGUI.lua
@@ -13,56 +13,66 @@ function evogui.update_gui()
         for i, player in ipairs(game.players) do
             evogui.create_or_update(player)
 
-            if not player.gui.top.evoGUI then
-                return
-            end
+            if not player.gui.top.evoGUI then return end
 
-            if player.gui.top.evoGUI.evolution_factor then
-                evogui.update_evolution(player.gui.top.evoGUI.evolution_factor)
+            local sensors = player.gui.top.evoGUI.sensors
+            if not sensors then return end
+
+            if sensors.evolution_factor then
+                evogui.update_evolution(sensors.evolution_factor)
             end
-            if player.gui.top.evoGUI.play_time then
-                evogui.update_play_time(player.gui.top.evoGUI.play_time)
+            if sensors.play_time then
+                evogui.update_play_time(sensors.play_time)
             end
-            if player.gui.top.evoGUI.first_line.day_time then
-                evogui.update_day_time(player.gui.top.evoGUI.first_line.day_time)
+            if sensors.day_time then
+                evogui.update_day_time(sensors.day_time)
             end
         end
     end
 end
 
 function evogui.create_or_update(player)
-    if not player.gui.top.evoGUI  then
-        player.gui.top.add{type="frame", name="evoGUI", direction = "vertical"}
-        player.gui.top.evoGUI.add{type="frame", name="first_line", direction = "horizontal", style="naked_frame_style"}
-        player.gui.top.evoGUI.first_line.add{type="button", name="evoGUI_size", caption="-", style="evoGUI_small_button_style"}
-        player.gui.top.evoGUI.first_line.add{type="label", name="day_time"}
-        player.gui.top.evoGUI.add{type="label", name="evolution_factor"}
-        player.gui.top.evoGUI.add{type="label", name="play_time"}
+    local root = player.gui.top.evoGUI
+    local destroyed = false
+    if root and (not global.evoGUI or not global.evoGUI.version or global.evoGUI.version ~= "0.3.0") then
+        player.gui.top.evoGUI.destroy()
+        destroyed = true
     end
 
-    if not player.gui.top.evoGUI.first_line or
-        not player.gui.top.evoGUI.first_line.evoGUI_size or
-        not player.gui.top.evoGUI.first_line.day_time then
-        player.gui.top.evoGUI.destroy()
+    if not root or destroyed then
+        local root = player.gui.top.add{type="frame", name="evoGUI", direction = "horizontal", style="outer_frame_style"}
+
+        local action_buttons = root.add{type="flow", name="action_buttons", direction = "vertical", style="description_flow_style"}
+        action_buttons.add{type="button", name="evoGUI_size", caption="-", style="evoGUI_small_button_style"}
+        action_buttons.add{type="button", name="evoGUI_settings", caption="s", style="evoGUI_small_button_style"}
+
+        local sensors = root.add{type="flow", name="sensors", direction = "vertical", style="description_flow_style"}
+        sensors.add{type="label", name="day_time"}
+        sensors.add{type="label", name="evolution_factor"}
+        sensors.add{type="label", name="play_time"}
+
+        if not global.evoGUI then global.evoGUI = {} end
+        global.evoGUI.version = "0.3.0"
     end
 end
 
 function evogui.on_click.evoGUI_size(event)
     local player = game.get_player(event.player_index)
 
-    if player.gui.top.evoGUI.evolution_factor ~= nil then
+    local sensors = player.gui.top.evoGUI.sensors
+    if sensors.evolution_factor ~= nil then
         -- hide the extra bits
-        player.gui.top.evoGUI.evolution_factor.destroy()
-        player.gui.top.evoGUI.play_time.destroy()
-        player.gui.top.evoGUI.first_line.evoGUI_size.caption = "+"
+        sensors.evolution_factor.destroy()
+        sensors.play_time.destroy()
+        player.gui.top.evoGUI.action_buttons.evoGUI_size.caption = "+"
     else
         -- show the extra bits
-        player.gui.top.evoGUI.add{type="label", name="evolution_factor"}
-        evogui.update_evolution(player.gui.top.evoGUI.evolution_factor)
+        sensors.add{type="label", name="evolution_factor"}
+        evogui.update_evolution(sensors.evolution_factor)
 
-        player.gui.top.evoGUI.add{type="label", name="play_time"}
-        evogui.update_play_time(player.gui.top.evoGUI.play_time)
+        sensors.add{type="label", name="play_time"}
+        evogui.update_play_time(sensors.play_time)
 
-        player.gui.top.evoGUI.first_line.evoGUI_size.caption = "-"
+        player.gui.top.evoGUI.action_buttons.evoGUI_size.caption = "-"
     end
 end
