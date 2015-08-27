896d0123835cba1f3887725c53aef915b7b408cb
Octav Sandulescu <github@narc.ro>
2015-08-27 07:45:37 +0000

    Add player index and direction to player locations
    Closes #11, but it's getting a little long now. Need #13 soon.

--- value_sensors/player_locations.lua
+++ value_sensors/player_locations.lua
@@ -14,13 +14,50 @@ function sensor:create_ui(owner)
     end
 end
 
+
+local function directions(source, destination)
+    -- Directions to or from positionless things? Hrm.
+    if not source.position or not destination.position then return '?' end
+
+    local delta_x = destination.position.x - source.position.x
+    local delta_y = destination.position.y - source.position.y
+
+    if math.abs(delta_x) > math.abs(delta_y) then
+        if delta_x < -1 then
+            return '<'
+        elseif delta_x > 1 then
+            return '>'
+        else
+            return '='
+        end
+    else
+        if delta_y < -1 then
+            return '^'
+        elseif delta_y > 1 then
+            return 'v'
+        else
+            return '='
+        end
+    end
+end
+
+
 function sensor:update_ui(owner)
     for _, p in ipairs(game.players) do
         if owner[self.name].player_list[p.name] == nil then
             owner[self.name].player_list.add{type="label", name=p.name}
         end
-        local desc = string.format("%s @(%d, %d on %s)", p.name,
-            p.position.x, p.position.y, p.surface.name)
+
+        local direction = '?'
+        local current_player = game.get_player(owner.player_index)
+        if p == current_player then
+            direction = ''
+        elseif current_player then
+            direction = directions(current_player, p)
+        end
+
+        local desc = string.format("(%d) %s @(%d, %d on %s) %s", p.index,
+            p.name, p.position.x, p.position.y, p.surface.name, direction)
         owner[self.name].player_list[p.name].caption = desc
     end
 end
