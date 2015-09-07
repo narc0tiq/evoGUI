8dc8ed50ba52510fcc92c526abac2cdae9a21d70
Octav Sandulescu <github@narc.ro>
2015-09-07 08:32:43 +0000

    Add time-of-day settings: rounding and day number
    Closes #19, but probably wants #20 before it's really finished.
    
    On a similar note, there is almost certainly a less copy/pasty way to do
    the per-sensor settings bits.

--- value_sensors/day_time.lua
+++ value_sensors/day_time.lua
@@ -1,6 +1,18 @@
 require "template"
 
+if not evogui.on_click then evogui.on_click = {} end
 local sensor = ValueSensor.new("day_time")
+
+
+function sensor:update_ui(owner)
+    local player = game.get_player(owner.player_index)
+    local sensor_settings = global.evogui[player.name].sensor_settings[self.name]
+
+    self.settings = sensor_settings
+
+    owner[self.name].caption = self:get_line()
+end
+
 
 function sensor:get_line()
     -- 0.5 is midnight; let's make days *start* at midnight instead.
@@ -9,13 +21,73 @@ function sensor:get_line()
     local day_time_minutes = math.floor(day_time * 24 * 60)
     local day_time_hours = math.floor(day_time_minutes / 60)
 
-    local rounded_minutes = day_time_minutes - (day_time_minutes % 15)
+    local display_minutes = day_time_minutes
+    if self.settings.minute_rounding then
+        display_minutes = day_time_minutes - (day_time_minutes % 15)
+    end
 
     local brightness = math.floor((1 - game.darkness) * 100)
 
-    return {self.format_key,
-            string.format("%d:%02d", day_time_hours, rounded_minutes % 60),
-            string.format("%d%%", brightness)}
+    if self.settings.show_day_number then
+        local day_number = 1 + ((game.tick + 12500) / 25000)
+        return {"sensor.day_time.day_format",
+                string.format("%d:%02d", day_time_hours, display_minutes % 60),
+                string.format("%d", day_number),
+                string.format("%d%%", brightness)}
+    else
+        return {self.format_key,
+                string.format("%d:%02d", day_time_hours, display_minutes % 60),
+                string.format("%d%%", brightness)}
+    end
 end
+
+
+function sensor:close_settings_gui(player_index)
+    local player = game.get_player(player_index)
+    local root_name = self:settings_root_name()
+
+    player.gui.center[root_name].destroy()
+
+    if self.settings_gui_closed then self.settings_gui_closed(player_index) end
+end
+
+
+function sensor:settings_gui(player_index)
+    local player = game.get_player(player_index)
+    local sensor_settings = global.evogui[player.name].sensor_settings[self.name]
+    local root_name = self:settings_root_name()
+
+    local root = player.gui.center.add{type="frame",
+                                       name=root_name,
+                                       direction="vertical",
+                                       caption={"sensor.day_time.settings.title"}}
+    root.add{type="checkbox", name="evogui_show_day_number",
+             caption={"sensor.day_time.settings.show_day_number"},
+             state=sensor_settings.show_day_number}
+    root.add{type="checkbox", name="evogui_minute_rounding",
+             caption={"sensor.day_time.settings.minute_rounding"},
+             state=sensor_settings.minute_rounding}
+
+    local btn_close = root.add{type="button", name="evogui_custom_sensor_close", caption={"settings_close"}}
+    evogui.on_click[btn_close.name] = function(event) self:close_settings_gui(player_index) end
+end
+
+
+function evogui.on_click.evogui_show_day_number(event)
+    local player = game.get_player(event.player_index)
+    local sensor_settings = global.evogui[player.name].sensor_settings["day_time"]
+
+    sensor_settings.show_day_number = event.element.state
+end
+
+
+function evogui.on_click.evogui_minute_rounding(event)
+    local player = game.get_player(event.player_index)
+    local sensor_settings = global.evogui[player.name].sensor_settings["day_time"]
+
+    sensor_settings.minute_rounding = event.element.state
+end
+
+
 
 ValueSensor.register(sensor)
